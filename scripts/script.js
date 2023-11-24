e48c2e77c984177d2e77a427e26414446071d151
Francisco Zuidwijk <electrocisko@gmail.com>
2023-11-24 09:35:19 -0300

    validation form

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,62 @@
 const form = document.getElementById("form");
+const firstName = document.getElementById("fname");
+const lastName = document.getElementById("lname");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const warningFirstName = document.getElementById("fnameP");
+const warningLastName = document.getElementById("lnameP");
+const warningEmail = document.getElementById("emailP");
+const warningPassword = document.getElementById("passwordP");
 
 let regEx = /\S+@\S+\.\S+/; //short regexp for email
+let user;
+let formCompleted = false;
 
 const handleSubmit = (evt, form) => {
   evt.preventDefault();
   let data = new FormData(form);
-  const user = Object.fromEntries(data.entries());
-
-  for (const key in user) {
-    if (Object.hasOwnProperty.call(user, key)) {
-      const element = user[key];
-      if (!element) {
-        console.log("Campo vacio: " + key);
-        const input = document.getElementById(key);
-        input.classList.add("error");
-        input.placeholder = "incompleto";
-        console.log(input);
-      }
-    }
-  }
-  let validMail = regEx.test(user.email);
-console.log('Valido Mail: ' + validMail);
+  user = Object.fromEntries(data.entries());
+  validateForm(user);
+  if (formCompleted) {
+    form.reset();
+    formCompleted = false;
+  } 
 };
 
-
+function validateForm(user) {
+  if (!user.fname) {
+    firstName.classList.add("error");
+    warningFirstName.classList.remove('hide');
+  } else {
+    firstName.classList.remove("error");
+    warningFirstName.classList.add('hide');
+  }
+  if (!user.lname) {
+    lastName.classList.add("error");
+    warningLastName.classList.remove('hide');
+  } else {
+    lastName.classList.remove("error");
+    warningLastName.classList.add('hide');
+  }
+  if (!user.email) {
+    email.classList.add("error");
+    warningEmail.classList.remove('hide');
+  } else {
+    email.classList.remove("error");
+    warningEmail.classList.add('hide');
+  }
+  if (!user.password) {
+    password.classList.add("error");
+    warningPassword.classList.remove('hide');
+  } else {
+    password.classList.remove("error");
+    warningPassword.classList.add('hide');
+  }
+  if (user.fname && user.lname && user.email && user.password) {
+    formCompleted = true;
+  }
+}
 
 form.addEventListener("submit", (e) => {
   handleSubmit(e, e.target);
-  form.reset();
 });
